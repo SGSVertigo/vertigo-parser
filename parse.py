ef219c5456936c6a9f570e7911b81ce48bc8113c
Jon Sowman <jon@jonsowman.com>
2017-07-10 22:18:22 +0100

    extract gps data ok

--- parse.py
+++ parse.py
@@ -26,21 +26,34 @@ while True:
     idx = MSG_LEN * i
     try:
         packet = raw[idx : idx + MSG_LEN]
-        p_ts = binascii.hexlify(packet[3] + packet[2] + packet[1] + packet[0])
-        p_typ = binascii.hexlify(packet[4])
+        # Extract timestamp
+        p_ts = int(binascii.hexlify(packet[3] + packet[2] + packet[1] +
+            packet[0]), 16)
+        # Extract message type
+        p_typ = int(binascii.hexlify(packet[4]), 16)
+        if p_typ == 1:
+            # Extract GPS data
+            gps_lon_b = packet[8] + packet[7] + packet[6] + packet[5]
+            gps_lon = float(struct.unpack(">i", gps_lon_b)[0] / 1e7)
+            gps_lat_b = packet[12] + packet[11] + packet[10] + packet[9]
+            gps_lat = float(struct.unpack(">i", gps_lat_b)[0] / 1e7)
+            gps_alt_b = packet[16] + packet[15] + packet[14] + packet[13]
+            gps_alt = float(struct.unpack(">i", gps_alt_b)[0] / 1e3)
+            print "Lon/lat/alt = %f/%f/%f" % (gps_lon, gps_lat, gps_alt)
+        elif p_typ == 2:
+            pass
+        elif p_typ == 3:
+            pass
+        else:
+            print "huh?"
         p_d0 = binascii.hexlify(packet[6] + packet[5])
         p_d1 = binascii.hexlify(packet[8] + packet[7])
         p_d2 = binascii.hexlify(packet[10] + packet[9])
         p_d3 = binascii.hexlify(packet[12] + packet[11])
         p_d4 = binascii.hexlify(packet[14] + packet[13])
         p_d5 = binascii.hexlify(packet[16] + packet[15])
-        print "%dms | %02d | %04X,%04X,%04X,%04X,%04X,%04X" % (int(p_ts, 16), int(p_typ, 16),
-                int(p_d0, 16),
-                int(p_d1, 16),
-                int(p_d2, 16),
-                int(p_d3, 16),
-                int(p_d4, 16),
-                int(p_d5, 16))
+
+        # Next log_msg_t
         i += 1
     except IndexError:
         print("Finished parsing %s" % logfile)
